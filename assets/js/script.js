61d6bf3b8f74f663440e7bb2d335d88858cfc66b
Gustav0Luiz <gustavo9661@gmail.com>
2023-02-13 21:53:02 -0300

    commit inicial

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+const input = document.querySelector('.login-input');
+const button = document.querySelector('.login-button');
+const form = document.querySelector('.login-form')
+
+
+const validateInput = ( { target } ) => {
+    if (target.value.length >= 1)  {
+        button.removeAttribute('disabled')
+        return;
+    }
+    button.setAttribute('disabled','')
+}
+
+const HandleSubmit = (e) => {
+    e.preventDefault();
+    localStorage.setItem('Jogador', input.value);
+    window.location = 'assets/pages/game.html';
+}
+
+input.addEventListener('input', validateInput);
+
+form.addEventListener('submit', HandleSubmit);
